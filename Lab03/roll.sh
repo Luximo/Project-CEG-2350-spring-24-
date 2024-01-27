a8b4d3d5cb77c3f09d930d0713dddff9e437399e
Moses Otuvedo <56436632+Luximo@users.noreply.github.com>
2024-01-27 18:39:58 -0500

    Testing step 2

--- Lab03/roll.sh
+++ Lab03/roll.sh
@@ -11,8 +11,5 @@ if [ $number -lt 1 ]; then
   exit 1
 fi
 
-# Generate a random number between 1 and the user's number using the $RANDOM variable and the modulo operator
-random=$(( $RANDOM % $number + 1 ))
-
 # Print the output to the standard output stream
 echo "You rolled a $random"
