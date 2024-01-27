ae16c598c69e85413db90e5cae142aa44484460a
Moses Otuvedo <56436632+Luximo@users.noreply.github.com>
2024-01-27 18:37:15 -0500

    Step 2 testing

--- Lab03/roll.sh
+++ Lab03/roll.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-# This is a script that outputs a random number between 1 and 20...
+# This is a script that outputs a random number between 1 and the user's number
+# The script should be executable by the user
 
-# Output a random number between 1 and 20...
-number=$(( $RANDOM % 20 + 1 ))
+# Prompt the user for a number and store it in a variable
+read -p "Enter a number: " number
 
-# Print the output to the standard output stream...
-echo "You rolled a $number"
+# Check if the number is less than 1 and output an error message if so
+if [ $number -lt 1 ]; then
+  echo "Invalid number. Please enter a positive integer."
+  exit 1
+fi
+
+# Generate a random number between 1 and the user's number using the $RANDOM variable and the modulo operator
+random=$(( $RANDOM % $number + 1 ))
+
+# Print the output to the standard output stream
+echo "You rolled a $random"
