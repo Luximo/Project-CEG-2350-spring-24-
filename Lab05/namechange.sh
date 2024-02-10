40c373024bf915e2bca27b33861ecce9cd0f70b4
Moses Otuvedo <56436632+Luximo@users.noreply.github.com>
2024-02-10 07:17:56 -0500

    Namescript step 1 testing

---
+++ Lab05/namechange.sh
@@ -0,0 +1,8 @@
+# Define a function called printHelp
+printHelp() {
+  # Print the usage message
+  echo "Usage: namechange -f find -r replace \"string to modify\""
+  # Print the description of the options
+  echo " -f The text to find in the filename"
+  echo " -r The replacement text for the new filename"
+}
