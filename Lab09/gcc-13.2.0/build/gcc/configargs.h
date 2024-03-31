a710db4b5af6a3b980202c9e3b1908874a293a15
Ubuntu <otuvedo.3@wright.edu>
2024-03-31 01:22:59 +0000

    Add comments to source code files for prototypetesting

---
+++ Lab09/gcc-13.2.0/build/gcc/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../configure --enable-languages=c,c++ --disable-multilib";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
