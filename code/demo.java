a49ada6eebe50fdacbca145969a0e602e28c3040
Ubuntu <otuvedo.3@wright.edu>
2024-04-22 21:41:13 +0000

    last week of finals with Kayleigh Duncan

---
+++ code/demo.java
@@ -0,0 +1,7 @@
+public class demo {
+	public static void main(String[] args) {
+		System.out.println("Hello world from Java!");
+		System.out.println("My new features!");
+		System.out.println("So happy!!!");
+	}
+}
